594808b8b3f575b273a9ec65bae41036510a1b33
Caique Silva <caiquesilva12@hotmail.com>
2025-04-12 16:42:57 -0300

    fix: Mongoose models and type definitions

--- src/models/CategoryModel.ts
+++ src/models/CategoryModel.ts
@@ -1,47 +1,14 @@
-import mongoose, { Model, Schema, Document } from 'mongoose';
+import mongoose, { Document, Model } from 'mongoose';
 import { ICategory } from '../types/Category';
 
-interface ICategoryDocument extends ICategory, Document {
-  _id: mongoose.Types.ObjectId;
-}
+interface ICategoryModel extends ICategory, Document {}
 
-class CategoryModel {
-  private schema: Schema;
-  private model: Model<ICategoryDocument>;
+const categorySchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  type: { type: String, required: true },
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }
+});
 
-  constructor() {
-    this.schema = new Schema<ICategoryDocument>(
-      {
-        name: { type: String, required: true },
-        user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
-        type: { type: String, enum: ['revenue', 'expense'], required: true }
-      },
-      {
-        timestamps: true,
-      }
-    );
+const CategoryModel: Model<ICategoryModel> = mongoose.model<ICategoryModel>('Category', categorySchema);
 
-    this.model = mongoose.model<ICategoryDocument>('Category', this.schema);
-  }
-
-  async create(categoryData: ICategory): Promise<ICategoryDocument> {
-    const category = await this.model.create(categoryData);
-    return category;
-  }
-
-  async getUserCategories(userId: mongoose.Types.ObjectId): Promise<ICategoryDocument[]> {
-    const categories = await this.model.find({ user: userId });
-    return categories;
-  }
-
-  async updateBudget(categoryId: mongoose.Types.ObjectId, budget: number): Promise<ICategoryDocument | null> {
-    const category = await this.model.findByIdAndUpdate(
-      categoryId,
-      { budget },
-      { new: true }
-    );
-    return category;
-  }
-}
-
-export default new CategoryModel(); 
+export default CategoryModel; 
--- src/models/UserModel.ts
+++ src/models/UserModel.ts
@@ -1,50 +1,17 @@
-import mongoose, { Model, Schema, Document } from 'mongoose';
+import mongoose, { Document, Model } from 'mongoose';
 import { IUser } from '../types/User';
 
-interface IUserDocument extends IUser, Document {
+interface IUserModel extends IUser, Document {
   _id: mongoose.Types.ObjectId;
 }
 
-class UserModel {
-  private schema: Schema;
-  private model: Model<IUserDocument>;
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  balance: { type: Number, default: 0 }
+});
 
-  constructor() {
-    this.schema = new Schema<IUserDocument>(
-      {
-        name: { type: String, required: true },
-        email: { type: String, required: true, unique: true },
-        password: { type: String, required: true },
-        balance: { type: Number, default: 0 },
-      },
-      {
-        timestamps: true,
-      }
-    );
+const UserModel: Model<IUserModel> = mongoose.model<IUserModel>('User', userSchema);
 
-    this.model = mongoose.model<IUserDocument>('User', this.schema);
-  }
-
-  async create(userData: IUser): Promise<IUserDocument> {
-    const user = await this.model.create(userData);
-    return user;
-  }
-
-  async findByEmail(email: string): Promise<IUserDocument | null> {
-    const user = await this.model.findOne({ email });
-    return user;
-  }
-
-  async findById(userId: mongoose.Types.ObjectId): Promise<IUserDocument | null> {
-    const user = await this.model.findById(userId);
-    return user;
-  }
-
-  public async updateBalance(userId: mongoose.Types.ObjectId, amount: number): Promise<void> {
-    await this.model.findByIdAndUpdate(userId, {
-      $inc: { balance: amount },
-    });
-  }
-}
-
-export default new UserModel();
+export default UserModel;
